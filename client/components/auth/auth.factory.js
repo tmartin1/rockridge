0547c7dfe7396bb3d42b2ab6ec5bb5416dd9ea69
Tim Martin <tmartin1@users.noreply.github.com>
2015-02-12 12:13:46 -0800

    Update auth.factory.js

--- client/components/auth/auth.factory.js
+++ client/components/auth/auth.factory.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('rockridge')
-  .factory('Auth', function($location, $rootScope, $http, User, $cookieStore, $q) {
-    //
-  });
+.factory('Auth', function($location, $rootScope, $http, User, $cookieStore, $q) {
+  //
+});
