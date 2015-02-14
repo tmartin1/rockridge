024f42bc13743445b6f48a1394f671608f2355e3
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-02-14 14:16:49 -0800

    Add basic user authentication.

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
-  host: process.env.PORT || 'localhost',
+  host: process.env.HOST || 'localhost',
   port: process.env.PORT || 8080,
-  orient: {}
+  db: {
+    host: process.env.ORIENTDB_HOST,
+    port: process.env.ORIENTDB_PORT,
+    user: process.env.ORIENTDB_USER,
+    password: process.env.ORIENTDB_PASSWORD
+  }
 }
