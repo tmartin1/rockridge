fd62b0a8e94d836158160df6d05e4d4f64b20a7a
Tim Martin <timothy.m.martin@gmail.com>
2015-02-17 13:21:01 -0800

    Add notes and template for queiries objects.

--- client/app/plan-builder/basics/basics.controller.js
+++ client/app/plan-builder/basics/basics.controller.js
@@ -3,4 +3,12 @@
 angular.module('rockridge')
 .controller('BasicsCtrl', function($scope) {
   // assign collected info to $scope.plan, defined in parent state: plan-builder.controller.js
+
+  // $scope.queries must be an array of question objects in the order that they should appear to 
+  // the user. Question objects have a specific format that must be followed. For more info, see 
+  // client/components/question/questions.directive.js
+  $scope.queries = [
+    { question:'What is your name?', type:'text', bind:'name', options:null, next:null },
+    { question:'Are you married?', type:'select', bind:'married', options:['Yes','No'], next:function($scope){ return $scope.plan.married ? 2 : 6 } } // Example, if married, go to question 3, else skip to question 6.
+  ];
 });
--- client/app/plan-builder/nws/nws.controller.js
+++ client/app/plan-builder/nws/nws.controller.js
@@ -3,4 +3,12 @@
 angular.module('rockridge')
 .controller('NwsCtrl', function($scope) {
   // assign collected info to $scope.plan, defined in parent state: plan-builder.controller.js
+
+  // $scope.queries must be an array of question objects in the order that they should appear to 
+  // the user. Question objects have a specific format that must be followed. For more info, see 
+  // client/components/question/questions.directive.js
+  $scope.queries = [
+    {} // Enter question objects here
+  ];
+
 });
