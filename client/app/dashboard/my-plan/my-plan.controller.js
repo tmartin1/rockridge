12de69e598aadcaaeb754e7a7a9d79f588d567dd
Tim Martin <timothy.m.martin@gmail.com>
2015-02-19 16:38:01 -0800

    Establish my-plan section view routes and files.

--- client/app/dashboard/my-plan/my-plan.controller.js
+++ client/app/dashboard/my-plan/my-plan.controller.js
@@ -5,8 +5,28 @@
     - allow the user to update their plan
 */
 angular.module('rockridge')
-.controller('MyPlanCtrl', function($scope, $state) {
+.controller('MyPlanCtrl', function($rootScope, $scope, $state) {
+  
   // Demo for the example, this will have to be dynamic (allow user to add tab for college savings, etc.) 
   $scope.user = $scope.user || {};
   $scope.user.plan = ['Net Worth', 'Budget', 'Insurance', 'Retirement'];
+  
+  // Default my-plan view to 'overview'
+  $scope.currentView = 'Overview';
+
+  // Different dispay options for user's plan.
+  $scope.planDisplay = {
+    'Overview': 'app/dashboard/my-plan/views/overview.html',
+    'Net Worth': 'app/dashboard/my-plan/views/nws.html',
+    'Budget': 'app/dashboard/my-plan/views/msa.html',
+    'Insurance': 'app/dashboard/my-plan/views/insurance.html',
+    'Retire': 'app/dashboard/my-plan/views/retire.html',
+    'addMore': 'app/dashboard/my-plan/views/addmore.html'
+  };
+
+  // Set current view inside of my-plan.
+  $scope.setView = function(section) {
+    $scope.currentView = section;
+  };
+
 });
