4e6dbed79c05a7d6991e6131560f7d3b02cb735a
Tim Martin <timothy.m.martin@gmail.com>
2015-02-20 17:36:24 -0800

    Add budget questions.

--- client/app/plan-builder/msa/msa.controller.js
+++ client/app/plan-builder/msa/msa.controller.js
@@ -18,45 +18,52 @@ angular.module('rockridge')
   // client/components/question/questions.directive.js
   $scope.queries = [
     {
-      title: '',
-      question: "",
-      bind: '',
-      type: '',
-      options: '',
-      fields: '',
-      condition: '',
-      subqueries: '',
-      template: ''
+      title: 'Payroll Deductions',
+      type: 'multi',
+      subqueries: [
+        {
+          question: "Enter the total deductions from your paycheck (not including retirement contributions):",
+          bind: 'userPayrollDeductions',
+          type:'number'
+        }, {
+          question: "Enter the total deductions from " + $scope.plan.spouseFirstName + "'s' paycheck (not including retirement contributions):",
+          bind: 'spousePayrollDeductions',
+          type: 'number',
+          condition: 'maritalStatus'
+        }, {
+          question:"Please enter any other tax deductions here:",
+          type:'number',
+          bind:'otherDeductions'
+        }
+      ]
     }, {
-      title: '',
-      question: "",
-      bind: '',
-      type: '',
-      options: '',
-      fields: '',
-      condition: '',
-      subqueries: '',
-      template: ''
+      title: 'Fixed Expenses',
+      type: 'table',
+      bind: 'fixedExpenses',
+      question: 'Fixed expenses are the ones that you would consider necessary to get by (mortgage payments, groceries, utilities, child care expenses, etc.).',
+      fields: [
+        {
+          label: 'Fixed Expense Name',
+          type: 'text'
+        }, {
+          label: 'Monthly Expense',
+          type: 'number'
+        }
+      ]
     }, {
-      title: '',
-      question: "",
-      bind: '',
-      type: '',
-      options: '',
-      fields: '',
-      condition: '',
-      subqueries: '',
-      template: ''
-    }, {
-      title: '',
-      question: "",
-      bind: '',
-      type: '',
-      options: '',
-      fields: '',
-      condition: '',
-      subqueries: '',
-      template: ''
+      title: 'Flexible Expenses',
+      type: 'table',
+      bind: 'flexibleExpenses',
+      question: 'Flexible expenses are the ones that you could live without if you needed to (travel, dining out, etc.) As you may not travel every month, try to approximate by taking your annual travel expense and dividing it by twelve.',
+      fields: [
+        {
+          label: 'Flexible Expense Name',
+          type: 'text'
+        }, {
+          label: 'Monthly Expense',
+          type: 'number'
+        }
+      ]
     }
   ];
 
