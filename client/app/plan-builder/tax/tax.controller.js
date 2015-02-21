635e859c6f236d5636b89b8540c1362f26f1e2e5
Tim Martin <timothy.m.martin@gmail.com>
2015-02-20 18:06:10 -0800

    Fix typos and remove console.logs.

--- client/app/plan-builder/tax/tax.controller.js
+++ client/app/plan-builder/tax/tax.controller.js
@@ -11,7 +11,7 @@ Needed information:
 angular.module('rockridge')
 .controller('TaxProjectionCtrl', function($scope) {
   // assign collected info to $scope.plan, defined in parent state: plan-builder.controller.js
-  console.log($scope.plan);
+  
   // $scope.queries must be an array of question objects in the order that they should appear to
   // the user. Question objects have a specific format that must be followed. For more info, see
   // client/components/question/questions.directive.js
