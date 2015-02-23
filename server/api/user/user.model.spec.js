4cb5b277ab7f93f58fdc30fb689227f1ba8c600c
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-02-23 11:37:27 -0800

    Fixes User method tests

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -2,20 +2,6 @@ var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
-
-var oriento = require('oriento');
-var config = require('../../config/config');
-var bcrypt = require('bcrypt');
-
-var server = oriento(config.db);
-var db = server.use(
-  {
-  name: 'rockridge',
-  username: 'admin',
-  password: 'admin'
-  }
-);
-
 var User = require('./user.model');
 
 
@@ -36,40 +22,62 @@ describe('User Methods', function() {
   describe('create user', function() {
     before(function(done) {
       user.create('testuser@gmail.com', 'testPass', function(user) {
-        // console.log('create user return', user);
         account = user;
         done();
       });
     });
 
     it('should create a new user', function() {
-        // console.log('account', account);
         result = account.email;
         expect(result).to.equal('testuser@gmail.com');
     });
   });
 
+  var testUser;
+
   describe('find user by email', function() {
     it('should find a user by email', function() {
       user.findByEmail('testuser@gmail.com', function(val) {
+        testUser = val;
         result = val.email;
         expect(result).to.equal('testuser@gmail.com');
       });
     });
   });
 
-  describe('delete user', function() {
+
+
+  describe('find user by rid', function() {
+    var testRid;
 
     before(function(done) {
+      user.findByEmail('testuser@gmail.com', function(val) {
+        testRid = val['@rid'];
+        testRid = JSON.stringify(testRid);
+        var re = /cluster: (\d+), position: (\d+)/;
+        testRid = testRid.replace(re, "#$1:$2");
+        done();
+      });
+    });
+
+    it('should find a user by id', function() {
+      user.findById(testRid, function(user) {
+        var idResult = user.email;
+        expect(idResult).to.equal('testuser@gmail.com');
+      });
+    });
+  });
+
+
+  describe('delete user', function() {
+    before(function(done) {
       user.deleteByEmail('testuser@gmail.com', function(val) {
-        // if (val) { console.log('number Users deleted', val); };
       done();
       });
     });
 
     it('should delete a user', function(done) {
       user.findByEmail('testuser@gmail.com', function(val) {
-        // console.log('last test', val);
         expect(val).to.equal(undefined);
         done();
       });
