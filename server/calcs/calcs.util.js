1bea4cd96969cc6281f5f1d53c3e7174ecbc438a
Tim Martin <timothy.m.martin@gmail.com>
2015-02-23 12:41:17 -0800

    Add folder and file structure for tax and retirement calcs.

---
+++ server/calcs/calcs.util.js
@@ -0,0 +1,19 @@
+// For maintaining and organizing federal income tax variables.
+
+'use strict';
+
+module.exports = {
+
+  // Standard tax bracket rates and limits.
+  var federalBrackets = {};
+
+  // Variables for deductions and exemptions.
+  var deductions = {};
+
+  // FICA variables and limits.
+  var fica = {};
+
+  // Alternative Minimum Tax variables.
+  var amt = {};
+
+}
