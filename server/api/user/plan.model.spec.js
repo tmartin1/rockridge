b9f0db05877c442247745c6138546e93b3d4d903
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-02-23 17:19:29 -0800

    Adds working tests for Plan creation/finding/deletion

---
+++ server/api/user/plan.model.spec.js
@@ -0,0 +1,114 @@
+// Testing
+var chai = require('chai');
+var expect = chai.expect;
+
+// User and Plan methods
+var Plan = require('./plan.model');
+var User = require('./user.model');
+
+// OrientDB
+var oriento = require('oriento');
+var config = require('../../config/config');
+var server = oriento(config.db);
+var db = server.use({
+      name: 'rockridge',
+      username: 'admin',
+      password: 'admin'
+    });
+
+var plan = new Plan();
+var user = new User();
+
+
+// Test user functions
+
+describe('Plan Methods', function() {
+  var testUser;
+  var testUserRid;
+  var planData = {};
+  planData.name = 'Test fund';
+  var result = null;
+  var edge = null;
+
+  before(function(done) {
+    user.create('planTest@gmail.com', 'test', function(user) {
+      testUser = user;
+      testUserRid = '#' + user['@rid']['cluster'] + ':' + user['@rid']['position'];
+      done();
+    });
+  });
+
+  // Test create methods
+  describe('Create methods', function() {
+    before(function(done) {
+      plan.create(testUserRid, planData, function(thisPlan, thisEdge) {
+        result = thisPlan;
+        edge = thisEdge;
+        done();
+      });
+    });
+
+  it('should create a new plan', function() {
+    expect(result.data.name).to.equal('Test fund');
+  });
+
+  it('should create a link to the associated user', function() {
+    edgeOut = '#' + edge.out.cluster + ':' + edge.out.position;
+    expect(edgeOut).to.equal(testUserRid);
+  });
+
+  it('should create a link to the new plan', function() {
+    edgeIn = '#' + edge.in.cluster + ':' + edge.in.position;
+    resultRid = '#' + result['@rid'].cluster + ':' + result['@rid'].position;
+    expect(edgeIn).to.equal(resultRid);
+    });
+  });
+
+
+  // Test find method
+  describe('Find method', function() {
+    before(function(done) {
+      result = null;
+      plan.findByUserRid(testUserRid, function(plan) {
+        result = plan;
+        done();
+      });
+    });
+
+    it("should find a user's plan by user RID", function() {
+      resultRid = '#' + result['in_'].cluster + ':' + result['in_'].position;
+      expect(resultRid).to.equal(testUserRid);
+    });
+  });
+
+
+  // Test delete methods
+  describe('Delete methods', function() {
+    before(function(done) {
+      result = null;
+      var expandedUserRid = testUser['@rid'];
+      plan.deleteByUserRid(expandedUserRid, function(total) {
+        // console.log('deleted ' + total + ' plans');
+      });
+      done();
+    });
+
+    it("should delete the plan linked to a user's RID", function(done) {
+      plan.findByUserRid(testUserRid, function(plan) {
+        expect(plan).to.equal(undefined);
+        done();
+      });
+    });
+  });
+
+
+  // Cleanup: Delete test user
+  after(function(done) {
+    user.deleteByEmail('planTest@gmail.com', function(val) {
+      console.log('deleted', val);
+    });
+    // plan.deleteByRid(resultPlan['@rid']);
+    done();
+  });
+
+});
