b74a2ce59d3a70d712ab4442e611fa43a019188e
Tim Martin <timothy.m.martin@gmail.com>
2015-02-25 16:46:16 -0800

    Initial tax projection display.

--- client/app/dashboard/my-plan/my-plan.controller.js
+++ client/app/dashboard/my-plan/my-plan.controller.js
@@ -8,7 +8,6 @@ angular.module('rockridge')
 .controller('MyPlanCtrl', function($rootScope, $scope, $state, Auth) {
 
   $scope.user = Auth.getCurrentUser();
-  $scope.plan = {};
   // $scope.plan = Auth.getUserPlan();
 
   // Define default my-plan view to 'overview'.
@@ -21,12 +20,13 @@ angular.module('rockridge')
   $scope.planDisplay = [
     { label: 'Net Worth', link: 'app/dashboard/my-plan/views/nws.html' },
     { label: 'Budget', link: 'app/dashboard/my-plan/views/msa.html' },
-    { label: 'Taxes', link: 'app/dashboard/my-plan/views/tax.html' },
+    { label: 'Tax Projection', link: 'app/dashboard/my-plan/views/tax.html' },
     { label: 'Insurance', link: 'app/dashboard/my-plan/views/insurance.html' },
-    { label: 'Retire', link: 'app/dashboard/my-plan/views/retire.html' },
+    { label: 'Retirement', link: 'app/dashboard/my-plan/views/retire.html' },
     { label: 'Personal Info', link: 'app/dashboard/my-plan/views/personal.html' },
     { label: 'More', link: 'app/dashboard/my-plan/views/addmore.html' }
   ];
+
 
   // Set current view inside of my-plan.
   $scope.setView = function(section) {
