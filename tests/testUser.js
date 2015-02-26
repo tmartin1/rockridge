59ce67bb5bbeb4c691b637cbcfd56deff4dc67b2
Tim Martin <timothy.m.martin@gmail.com>
2015-02-25 17:42:48 -0800

    Fix typo in test user file.

--- tests/testUser.js
+++ tests/testUser.js
@@ -1,9 +1,9 @@
 // This should match the model user template and be used for testing.
 
 var testUser = {
-  firstName: Test,
-  lastName: User,
-  email: test@test.com,
-  password: test,
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@test.com',
+  password: 'test',
   builderComplete: true
 }
