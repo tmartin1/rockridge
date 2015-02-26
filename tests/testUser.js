0751f27572b69a86c0a8fa279ca619954a178457
Tim Martin <timothy.m.martin@gmail.com>
2015-02-25 16:46:47 -0800

    Update testuser and tax calcs.

--- tests/testUser.js
+++ tests/testUser.js
@@ -2,9 +2,11 @@
 // The inputs in this object match the inputs in the example excel document for comparrison.
 
 var testUser = {
-  birthdate: new Date(1969, 05, 09),
+  birthdate: new Date(1969, 5, 9),
   charitableContributions: 10000,
-  children: Array[1],
+  children: [
+    { name: 'Child 1 Name', birthdate: new Date(2015, 2, 14) }
+  ],
   cityResident: 'San Francisco',
   dependents: 2,
   firstname: 'Test',
@@ -38,7 +40,7 @@ var testUser = {
     currentRate: 0.039,
     currentTerm: 30,
     initialBalance: 400000,
-    startDate: new Date(2006, 05, 01),
+    startDate: new Date(2006, 5, 1),
   },
   mortgages: [
     { asset: 'Mortgage', rate: 0.0325, balance: 400000 }
@@ -57,7 +59,7 @@ var testUser = {
   ],
   sameWorkResidence: 'true',
   spouse401kContribution: 19000,
-  spouseBirthdate: new Date(1996, 08, 06),
+  spouseBirthdate: new Date(1996, 8, 6),
   spouseCityResident: 'San Francisco',
   spouseEmployerDisabilityInsurance: undefined,
   spouseEmployerLifeInsurance: 50000,
@@ -91,5 +93,37 @@ var testUser = {
     { asset: 'Brokerage Account', value: 25000 },
     { asset: 'User 401k', value: 137000 },
     { asset: 'Spouse 401k', value: 10000 }
-  ]
+  ],
+  taxProjection: {
+    householdGross: 350000,
+    pretaxContributions: 34000,
+    agi: 316000,
+    deductions: {
+      standard: 12600,
+      itemized: {
+        localTaxes: 38930,
+        propertyTaxes: 3050,
+        mortgageInterest: 10280,
+        miscDeduction: 5000
+      },
+      maxApplicable: 52061
+    },
+    exemptions: {
+      claimed: 8000,
+      reductions: 720,
+      maxApplicable: 7280
+    },
+    taxableIncome: 256659,
+    projected: {
+      federal: 60391,
+      amt: 13339,
+      local: 38930,
+      fica: {
+        ss: 13547,
+        medicare: 5075,
+        addlMedicare: 900
+      }
+    },
+    netIncome: 143999
+  }
 };
