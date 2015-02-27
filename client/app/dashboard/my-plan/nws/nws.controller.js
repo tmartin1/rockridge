5397ff24cc75170ffc27323163fbd26aa27123ff
Tim Martin <timothy.m.martin@gmail.com>
2015-02-27 11:40:54 -0800

    Save before rebase.

--- client/app/dashboard/my-plan/nws/nws.controller.js
+++ client/app/dashboard/my-plan/nws/nws.controller.js
@@ -4,5 +4,20 @@
 
 angular.module('rockridge')
 .controller('NwsViewCtrl', function($scope) {
-  //
-});
+  $scope.plan.fixedAssets = $scope.plan.fixedAssets || {};
+})
+
+// Directive to display nws groups of items (fixed assets, variable assets, etc.)
+// .directive('nwsgroup', function() {
+//   return {
+    // restrict: 'E',
+    // scope: {
+    //   group: '=',
+    //   type: '@',
+    //   style: '@'
+    // },
+    // controller: function($scope) {
+    //   console.log($scope.group);
+    // },
+//     template: 'Group is: {{plan}}' // end of template.
+// });
