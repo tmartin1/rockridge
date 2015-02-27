391d8317d81522c59b0e815cba9b0c44f3978ba7
Tim Martin <timothy.m.martin@gmail.com>
2015-02-26 17:02:04 -0800

    Fix functionality with text and number types.

--- client/components/preview/preview.directive.js
+++ client/components/preview/preview.directive.js
@@ -1,17 +1,20 @@
 /* Directive for displaying input fields as togglable text/input elements.
 
   Directive parameters:
-    plan - Enables two-way data binding with the user's plan. This should (almost) always be plan="plan".
-    bind - Defines which property of the plan this field should represent.
+    object - Enables two-way data binding with the user's plan.
+             NOTE: This must be an object and the property you are binding to must be on the first level of this object.
+             object="plan" would work for 1st level plan items, like plan.spouseFirstName but not plan.mortgage.initialBalance
+             in that case you would have to define object="plan.mortgage"
+    property - Defines which property of the plan this field should represent. See above comment for details on nested objects.
     type - What kind of data type. Default type is 'text'.
     style - Define specific styles for the text and input field.
 
   How to use in the HTML template:
 
-  <preview plan="plan" bind="[PROPERTY_TO_BIND_TO]"></preview>
+  <preview object="plan" property="[PROPERTY_TO_BIND_TO]"></preview>
 
-  example:
-  <preview plan="plan" bind="firstname"></preview> <!-- will create a preview field bound to plan.firstname -->
+  The following example will create a preview field bound to plan.firstname:
+  <preview object="plan.mortgage" property="homeValue"></preview>
 
   You may also pass in style attribute if needed (ex. style="text-align:right")
 */
@@ -26,17 +29,18 @@ angular.module('rockridge')
   return {
     restrict: 'E',
     scope: {
-      plan: '=',
-      bind: '@',
+      object: '=',
+      property: '@',
       type: '@',
       style: '@'
     },
     controller: function($scope) {
-      
+
       // Switch between text field (read only) and input field (editable).
       $scope.toggle = function() {
         $scope.edit = !$scope.edit;
       };
+
       // Default edit icon to hidden.
       $scope.showIcon = false;
     },
