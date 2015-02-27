34fedf9bccfcab1a9c12b32cd4a2891791c3eb6d
Michael Staub <mikestaub22@gmail.com>
2015-02-27 12:22:01 -0800

    merge conflicts resolved

--- client/app/dashboard/my-plan/my-plan.controller.js
+++ client/app/dashboard/my-plan/my-plan.controller.js
@@ -16,6 +16,11 @@ angular.module('rockridge')
   // Set my-plan view to 'overview' if undefined.
   $scope.currentView = $scope.currentView || $scope.overview;
 
+  // Set current view inside of my-plan.
+  $scope.setView = function(section) {
+    $scope.currentView = section;
+  };
+
   // Different dispay options for user's plan.
   $scope.planDisplay = [
     { label: 'Net Worth', link: 'app/dashboard/my-plan/nws/nws.html' },
@@ -27,9 +32,98 @@ angular.module('rockridge')
     { label: 'More', link: 'app/dashboard/my-plan/addmore/addmore.html' }
   ];
 
-  // Set current view inside of my-plan.
-  $scope.setView = function(section) {
-    $scope.currentView = section;
-  };
+})
+.controller('dashboard.NetWorthCtrl', function($rootScope, $scope, $state, Auth) {
+})
+.controller('dashboard.BudgetCtrl', function($rootScope, $scope, $state, Auth) {
 
-});
+  // http://www.highcharts.com/demo/pie-drilldown
+  var topLevelSlices = {
+    'fixed expenses': null,
+    'flexible expenses': null,
+    'savings': null
+  };
+  var sliceData = [
+    {
+      drilldown: 'fixed expenses',
+      name: 'fixed expenses',
+      visible: true,
+      y: 40
+    },
+    {
+      drilldown: 'flexible expenses',
+      name: 'flexible expenses',
+      visible: true,
+      y: 30
+    },
+    {
+      drilldown: 'savings',
+      name: 'savings',
+      visible: true,
+      y: 10
+    }
+  ];
+  var drillDownSlices = [
+    {
+      name: 'fixed expenses',
+      id: 'fixed expenses',
+      data: [ 20, 60, 20]
+    },
+    {
+      name: 'flexible expenses',
+      id: 'flexible expenses',
+      data: [ 25, 50, 25]
+    },
+    {
+      name: 'savings',
+      id: 'savings',
+      data: [ 10, 70, 20]
+    }
+  ];
+
+  // Create the chart
+  $('#container').highcharts({
+    chart: {
+      type: 'pie'
+    },
+    title: {
+      text: 'Your Budget'
+    },
+    subtitle: {
+      text: 'click each section to see a detailed breakdown'
+    },
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}: {point.y:.1f}%'
+        }
+      }
+    },
+    tooltip: {
+      enabled: false,
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+    series: [{
+      name: 'top',
+      colorByPoint: true,
+      data: sliceData
+    }],
+    drilldown: {
+      series: drillDownSlices
+    }
+  });
+  // hide highcharts.com logo
+  $('text[text-anchor=end]').hide();
+})
+.controller('dashboard.TaxCtrl', function($rootScope, $scope, $state, Auth) {
+})
+.controller('dashboard.InsuranceCtrl', function($rootScope, $scope, $state, Auth) {
+})
+.controller('dashboard.RetirementCtrl', function($rootScope, $scope, $state, Auth) {
+})
+.controller('dashboard.PersonalInfoCtrl', function($rootScope, $scope, $state, Auth) {
+})
+.controller('dashboard.MoreCtrl', function($rootScope, $scope, $state, Auth) {
+})
