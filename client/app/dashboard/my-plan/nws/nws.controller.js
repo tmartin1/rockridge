9adb1a621927ebaa3d899144694535eb5cd02871
Tim Martin <timothy.m.martin@gmail.com>
2015-02-27 16:30:02 -0800

    Initial net worth statement layout.

--- client/app/dashboard/my-plan/nws/nws.controller.js
+++ client/app/dashboard/my-plan/nws/nws.controller.js
@@ -4,21 +4,48 @@
 
 angular.module('rockridge')
 .controller('NwsViewCtrl', function($scope) {
-  $scope.plan.fixedAssets = $scope.plan.fixedAssets || {};
 
-});
+  // If plan has a mortgage, then add primary residence to personal assets and mortgage information to plan.debts.other
+  (function addMortgageInfo() {
+    if ($scope.plan.hasMortgage) {
+      // addMortgageInfo
+      console.log('adding mortgage');
+    }
+  })();
+
+  // Total value of all assets
+  var calculateTotal = function(addFrom) {
+    var total = 0;
+    var addToTotal = function(subset) {
+      for (var item in subset) {
+        total += (item['value'] || item['balance'] || 0);
+      }
+    };
+    for (var subset in addFrom) {
+      addToTotal(addFrom[subset]);
+    }
+    return total;
+  };
+  $scope.totalAssets = calculateTotal($scope.plan.assets);
+  $scope.totalDebts = calculateTotal($scope.plan.debts);
+})
 
 // Directive to display nws groups of items (fixed assets, variable assets, etc.)
-// .directive('nwsgroup', function() {
-//   return {
-    // restrict: 'E',
-    // scope: {
-    //   group: '=',
-    //   type: '@',
-    //   style: '@'
-    // },
-    // controller: function($scope) {
-    //   console.log($scope.group);
-    // },
-//     template: 'Group is: {{plan}}' // end of template.
-// });
+.directive('nwsgroup', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      group: '=',
+      title: '@'
+    },
+    controller: function($scope) {
+      // Generate group subtotals
+      $scope.subtotal = 0;
+        // console.log($scope.group);
+      for (var i=0; i<$scope.group.length; i++) {
+        $scope.subtotal += $scope.group[i]['value'] || $scope.group[i]['balance'] || 0;
+      }
+    },
+    templateUrl: './app/dashboard/my-plan/nws/nwsGroupTemplate.html'
+  }
+});
