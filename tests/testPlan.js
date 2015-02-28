4a05f30ac07809e7c2e64cebbbfd203706f019c0
Tim Martin <timothy.m.martin@gmail.com>
2015-02-27 17:12:46 -0800

    Organize asset and debt objects in plan object.

--- tests/testPlan.js
+++ tests/testPlan.js
@@ -2,19 +2,45 @@
 // The inputs in this object match the inputs in the example excel document for comparrison.
 
 var testPlan = {
+  assets: {
+    fixed: [
+      { asset: 'Checking Account', value: 5000 },
+      { asset: 'Savings Account', value: 10000 }
+    ],
+    personal: [
+      { asset: 'Home', value: 500000 },
+      { asset: 'Auto', value: 40000 },
+      { asset: 'Other Stuff', value: 10000 }
+    ],
+    variable: [
+      { asset: 'Brokerage Account', value: 25000 },
+      { asset: 'User 401k', value: 137000 },
+      { asset: 'Spouse 401k', value: 10000 }
+    ],
+  },
   birthdate: new Date(1969, 5, 9),
   charitableContributions: 10000,
   children: [
     { name: 'Child 1 Name', birthdate: new Date(2015, 2, 14) }
   ],
   cityResident: 'San Francisco',
+  debts: {
+    creditCards: [
+      { asset: 'VISA', rate: 10.99, balance: 5000 },
+      { asset: 'Chase Credit Card', rate: 15.99, balance: 4000 },
+    ],
+    other: [
+      { asset: 'Auto Loan', rate: 0.9, balance: 35000 },
+      { asset: 'Disney Wedding Loan', rate: 9.5, balance: 41000 }
+    ],
+    studentLoans: [
+      { asset: 'Federal Stafford', rate: 6.8, balance: 150000 },
+      { asset: 'Federal Perkins', rate: 5.0, balance: 50000 }
+    ],
+  },
   dependents: 2,
   firstname: 'Test',
   filingStatus: 'married',
-  fixedAssets: [
-    { asset: 'Checking Account', value: 5000 },
-    { asset: 'Savings Account', value: 10000 }
-  ],
   fixedExpenses: [
     { asset: 'Mortgage', value: 1960 },
     { asset: 'Utilities', value: 450 },
@@ -31,32 +57,21 @@ var testPlan = {
   ],
   grossAnnualIncome: 250000,
   hasChildren: 'true',
+  hasMortgage: 'true',
   hasPrimaryResidence: 'true',
   lastname: 'User',
   maritalStatus: 'true',
   mortgage: {
     homeValue: 500000,
     currentBalance: 300000,
-    currentRate: 0.039,
+    currentRate: 3.9,
     currentTerm: 30,
     initialBalance: 400000,
     startDate: new Date(2006, 5, 1),
   },
-  mortgages: [
-    { asset: 'Mortgage', rate: 0.0325, balance: 400000 }
-  ],
-  otherDebts: [
-    { asset: 'Auto Loan', rate: 0.009, balance: 35000 },
-    { asset: 'Chase Credit Card', rate: 0.1599, balance: 4000 },
-    { asset: 'Disney Wedding Loan', rate: 0.095, balance: 41000 }
-  ],
+  netWorth: 57000,
   otherDeductions: 5000,
   pensionIncome: 0,
-  personalAssets: [
-    { asset: 'Home', value: 500000 },
-    { asset: 'Auto', value: 40000 },
-    { asset: 'Other Stuff', value: 10000 }
-  ],
   sameWorkResidence: 'true',
   spouse401kContribution: 19000,
   spouseBirthdate: new Date(1996, 8, 6),
@@ -73,10 +88,6 @@ var testPlan = {
   spouseRothContribution: 5500,
   spouseStateResident: 'CA',
   stateResident: 'CA',
-  studentLoans: [
-    { asset: 'Federal Stafford', rate: 6.8, balance: 150000 },
-    { asset: 'Federal Perkins', rate: 5.0, balance: 50000 }
-  ],
   targetRetirementAge: 55,
   targetRetirementIncome: 8000,
   user401kContribution: 15000,
@@ -89,11 +100,6 @@ var testPlan = {
   userIndividualLifeInsurance: 0,
   userPayrollDeductions: 2000,
   userRothContribution: 5500,
-  variableAssets: [
-    { asset: 'Brokerage Account', value: 25000 },
-    { asset: 'User 401k', value: 137000 },
-    { asset: 'Spouse 401k', value: 10000 }
-  ],
   taxProjection: {
     householdGross: 350000,
     pretaxContributions: 34000,
