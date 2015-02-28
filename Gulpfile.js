179a291a4971506eb22ff2bc6e5b3980b294e808
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-02-28 14:10:53 -0800

    Fixes known CircleCI testing issue.

--- Gulpfile.js
+++ Gulpfile.js
@@ -115,9 +115,9 @@ gulp.task('seed', function() {
 // Mocha for back-end tests
 gulp.task('mocha', function() {
   return gulp.src('server/**/*.spec.js')
-    .pipe(mocha({ reporter: 'nyan' }));
-    // .once('error', function() { process.exit(); })
-    // .once('end', function() { process.exit(); });
+    .pipe(mocha({ reporter: 'nyan' }))
+    .once('error', function() { process.exit(); })
+    .once('end', function() { process.exit(); });
 });
 
 // Karma for front-end tests
