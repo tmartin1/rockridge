5b8716dfe69635d5377891f76a1162c2aab25a24
Tim Martin <timothy.m.martin@gmail.com>
2015-03-02 13:48:15 -0800

    Add personal info fields for my-plan.

--- client/app/dashboard/my-plan/personal/personal.controller.js
+++ client/app/dashboard/my-plan/personal/personal.controller.js
@@ -4,5 +4,21 @@
 
 angular.module('rockridge')
 .controller('PersonalViewCtrl', function($scope) {
-  //
+  // TODO: Refactor the states list so it's not so redundant.
+  $scope.states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
+    'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+    'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV',
+    'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA',
+    'VT', 'WA', 'WI', 'WV', 'WY'
+  ];
+
+  $('.dropdown').dropdown({
+    transition: 'drop'
+  });
+
+  $scope.testLog = function() {
+    console.log($scope.plan.maritalStatus);
+  };
+  $scope.testLog();
+
 });
