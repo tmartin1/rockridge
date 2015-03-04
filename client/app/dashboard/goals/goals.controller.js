0e5bfab0956a862c23e80f099c50905176c56377
Tim Martin <timothy.m.martin@gmail.com>
2015-03-04 11:26:20 -0800

    Add ability to remove personal goals from the list.

--- client/app/dashboard/goals/goals.controller.js
+++ client/app/dashboard/goals/goals.controller.js
@@ -3,10 +3,26 @@
 /* The goals view should provide the following functionality:
     - display plan-defined goals
     - display user-defined goals
-    - allow user to create new, edit, update their goals 
+    - allow user to create new, edit, update their goals
     - allow user to mark their goal as 'on track', 'complete', etc.
 */
 angular.module('rockridge')
 .controller('GoalsCtrl', function($scope) {
-  //
+
+  // Marks a goal as complete or not.
+  $scope.checkGoal = function(goal, index) {
+    console.log(goal);
+    console.log(index);
+  };
+
+  // Creates a new goal.
+  $scope.createGoal = function() {
+    //
+  };
+
+  // Removes selected goal from the list.
+  $scope.removeGoal = function(index) {
+    $scope.plan.goals.user.splice(index,1);
+  };
+
 });
